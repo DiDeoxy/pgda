aff48dac55017f29c1192723e0026199c7f35ebb
Max H <whargrea@uoguelph.ca>
2019-04-13 13:02:36 -0400

    idk

--- R/calc_eh.R
+++ R/calc_eh.R
@@ -6,17 +6,17 @@
 #' @param genotypes a matrix of haploid markers as rows with alleles encoded as
 #' 0 and 2
 #'
-#' @importFrom parallel mclapply
+#' @importFrom parallel detectCores mclapply
 #' 
 #' @return a vector of expected heterozygosities of the markers
 #'
 #' @export
 
-calc_eh <- function (genotypes, cores) {
+calc_eh <- function (genotypes) {
   mclapply(1:nrow(genotypes), function (row) {
     n <- sum(genotypes[row, ] == 0 | 2)
     p <- sum(genotypes[row, ] == 0) / n
     q <- sum(genotypes[row, ] == 2) / n
     (n / (n - 1)) * (1 - (p^2 + q^2))
-  }, mc.cores = cores)
+  }, mc.cores = detectCores())
 }
