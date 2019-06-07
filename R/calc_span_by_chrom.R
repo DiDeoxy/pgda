1bccac0345a66c3dfb4a9effe69bd4ee37b368c9
Max Hargreaves <whargrea@uoguelph.ca>
2019-06-07 12:38:39 -0400

    diff should not be default

--- R/calc_span_by_chrom.R
+++ R/calc_span_by_chrom.R
@@ -1,4 +1,5 @@
-#' Calculate the difference between the max and min positions on a chromosome
+#' Calculate the length of each chrom, optionally subtract the min position from
+#' it
 #'
 #' @param snp_chrom a factor vector inidicating the chrom of each marker
 #' @param snp_pos a vector with the position of each snp on its chrom
@@ -7,11 +8,16 @@
 #'
 #' @export
 
-span_by_chrom <- function (snp_chrom, snp_pos) {
-  by(snp_pos, snp_chrom,
-    function(chrom_pos) {
-      max(chrom_pos) - min(chrom_pos)
-    }
-  ) %>% as.list() %>% unlist()
+span_by_chrom <- function (snp_chrom, snp_pos, diff = FALSE) {
+  if (diff) {
+    by(snp_pos, snp_chrom,
+      function(chrom_pos) {
+        max(chrom_pos) - min(chrom_pos)
+      }
+    ) %>% as.list() %>% unlist()
+  } else {
+    by(snp_pos, snp_chrom, max) %>% as.list() %>% unlist()
+  }
+  
 }
 
