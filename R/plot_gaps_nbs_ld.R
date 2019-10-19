88e65f48f22a779ab33057a4b1ad3f2efa0c120e
Max Hargreaves <whargrea@uoguelph.ca>
2019-10-19 19:49:47 +0000

    changed x axis text to horizontal

--- R/plot_gaps_nbs_ld.R
+++ R/plot_gaps_nbs_ld.R
@@ -70,16 +70,16 @@ plot_gaps_nbs_ld <- function(
     scale_colour_manual(values = brewer.pal(4, "Dark2")) +
     # causes some values to be removed
     scale_x_log10(breaks = c(0.01, 0.1, 1, 10), limits = c(NA, 30)) +
-    scale_y_log10(limits = c(NA, y_lim)) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+    scale_y_log10(limits = c(NA, y_lim))
+    # theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
   plots[[3]] <- gaps_ld %>%
     ggplot() +
     geom_freqpoly(aes(ld, colour = genome), size = 1) +
     scale_colour_manual(values = brewer.pal(4, "Dark2")) +
     # causes some values to be removed
     xlim(0, 1) +
-    scale_y_log10(limits = c(1, y_lim)) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+    scale_y_log10(limits = c(1, y_lim))
+    # theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
   # turn plot list into ggmatrix
   plots_matrix <- ggmatrix(
